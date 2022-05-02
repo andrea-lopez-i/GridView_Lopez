f4f059591dc3c60dc83e359bedceed67bd0b8a0b
FlutLab.io <>
2022-05-02 18:12:39 +0000

    Autosave

---
+++ lib/gridviewarticulos.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+} //widget con estado
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/celular.jpg",
+    "assets/images/calculadora.jpg",
+    "assets/images/cereso.jpg",
+    "assets/images/platicas.jpg",
+    "assets/images/policias.jpg",
+    "assets/images/tele.jpg",
+    "assets/images/teclado.jpg",
+    "assets/images/tablet.jpg",
+  ]; //Lista de imagenes
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("CERESO"),
+        ),
+        body: GridView.custom(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: const EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
