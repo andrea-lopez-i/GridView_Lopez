9974d249740a70615ee7cdabb5869c5167f9d580
FlutLab.io <>
2022-05-02 14:20:47 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Ej. GridView', // pestañaen web
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ), //tema color global
+        home: const MyHomePage());
+  }
+} // widget sin estado
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+} //widget con estado
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/celular.jpg",
+    "assets/images/calculadora.jpg",
+    "assets/images/cereso.jpg",
+    "assets/images/platicas.jpg",
+    "assets/images/policias.jpg",
+    "assets/images/tele.jpg",
+    "assets/images/teclado.jpg",
+    "assets/images/tablet.jpg",
+  ]; //Lista de imagenes
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: const EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
